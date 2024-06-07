2f790ddfa9bc404510ba118820922711f2d749b2
Vlone Me <francisntima@gmail.com>
2024-06-07 06:38:00 -0400

    Project Structure

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const dbConnection = require('./src/config/dbConfig');
 
 dbConnection()
+const port = 4000
 const server = express();
 
 server.use(express.json());
 
 
-server.listen(4000, () => {
-    console.log('[server] running at http://localhost:4000')
+server.listen(port, () => {
+    console.log(`[server] running at http://localhost:${port}`)
 })
